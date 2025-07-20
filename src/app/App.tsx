218fa2012b54cef776cea98e148fc4d836b25a11
Matvey Druzhinin <druzhininmd2005@gmail.com>
2025-07-21 01:25:35 +0500

    feat: sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import { TaskBoardPage } from '@pages/TaskBoardPage/TaskBoardPage';
 import { EditTaskPage } from '@pages/EditTaskPage/EditTaskPage';
 import { NewTaskPage } from '@pages/NewTaskPage/NewTaskPage';
+import { Box } from '@mui/material';
+import { Sidebar } from '@/widgets/Sidebar/Sidebar';
 
 function App() {
   return (
-          <Routes>
-            <Route path="/" element={<TaskBoardPage />} />
-            <Route path="/task/new" element={<NewTaskPage/>} />      
-            <Route path="/task/:id" element={<EditTaskPage/>} /> 
-          </Routes>
-  )
+    <Box display="flex">
+      <Sidebar />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Routes>
+          <Route path="/" element={<TaskBoardPage />} />
+          <Route path="/task/new" element={<NewTaskPage />} />
+          <Route path="/task/:id" element={<EditTaskPage />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
 }
 export default App
